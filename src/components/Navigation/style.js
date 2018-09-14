0bad595cad9e0c0365ff78dba17f344e8fa1c2c4
Dominik <dominik.slawkowski@gmail.com>
2018-09-14 22:45:27 +0200

    Dodanie nawigacji

---
+++ src/components/Navigation/style.js
@@ -0,0 +1,16 @@
+import { Menu } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+export const StyledMenu = styled(Menu)`
+   
+`;
+
+export const Logo = styled.span`
+    font-size: 24px;
+    color: white;
+    letter-spacing: 2px;
+`;
+
+export const StyledItem = styled.span`
+    color: black;
+`;
