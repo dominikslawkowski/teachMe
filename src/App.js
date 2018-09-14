0a8fce6243775dfaf56156153e5124a025c3f5a1
Dominik <dominik.slawkowski@gmail.com>
2018-09-14 20:29:20 +0200

    Add react router in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div className="container">
+            <Navigation />
+              <div className="main-container">
+                <Route exact path="/" component={Welcome} />
+                <Route path="/logowanie" component={Login} />
+                <Route path="/logowanie" component={Home} />
+                <Route path="/profil" component={User} />
+                <Route path="/pary" component={Maches} />               
+              </div>
+        </div>
+      </Router>
     );
   }
 }
