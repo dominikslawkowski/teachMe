8bd7a872ca17fd264d273aa74896ec1de95706fe
Dominik <dominik.slawkowski@gmail.com>
2018-09-14 21:50:21 +0200

    Poprawny semantic

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,6 @@
 import React, {Fragment} from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
+import { Dropdown, Icon, Menu, Segment } from 'semantic-ui-react';
 
 export class Navigation extends React.Component {
     render(){
@@ -20,7 +20,41 @@ export class Navigation extends React.Component {
         //     </Link>
         //   </Nav>
         <Fragment>
-        <Button width={100}>dominik</Button>
+            <Menu attached='top'>
+            <Dropdown item icon='wrench' simple>
+                <Dropdown.Menu>
+                <Dropdown.Item>
+                    <Icon name='dropdown' />
+                    <span className='text'>New</span>
+
+                    <Dropdown.Menu>
+                    <Dropdown.Item>Document</Dropdown.Item>
+                    <Dropdown.Item>Image</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown.Item>
+                <Dropdown.Item>Open</Dropdown.Item>
+                <Dropdown.Item>Save...</Dropdown.Item>
+                <Dropdown.Item>Edit Permissions</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Header>Export</Dropdown.Header>
+                <Dropdown.Item>Share</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+
+            <Menu.Menu position='right'>
+                <div className='ui right aligned category search item'>
+                <div className='ui transparent icon input'>
+                    <input className='prompt' type='text' placeholder='Search animals...' />
+                    <i className='search link icon' />
+                </div>
+                <div className='results' />
+                </div>
+            </Menu.Menu>
+            </Menu>
+
+            <Segment attached='bottom'>
+            <img src='/images/wireframe/paragraph.png' />
+            </Segment>
         </Fragment>
         );
     }
