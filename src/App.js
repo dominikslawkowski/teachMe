6c0c5150278612a85fbda3a49e5f9f71696410c8
Adam Oleszko <adamoleszko44@gmail.com>
2018-09-15 03:55:17 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { Navigation, Welcome, Login } from './components/index';
+import Maches from './components/Matches/index.js';
 
 class App extends Component {
   render() {
@@ -14,8 +15,8 @@ class App extends Component {
               <Route path="/logowanie" component={Login} />  
                 {/* 
                 <Route path="/główna" component={Home} />
-                <Route path="/profil" component={User} />
-                <Route path="/pary" component={Maches} />                */}
+                <Route path="/profil" component={User} /> */}
+                <Route path="/pary" component={Maches} />               
               </div>
         </div>
       </Router>
