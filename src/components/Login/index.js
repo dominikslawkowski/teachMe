8a3a492afcd1378e21f5575aab7ea463cac3b556
Dominik <dominik.slawkowski@gmail.com>
2018-09-15 07:04:28 +0200

    adasd

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Grid, Container, InputEmail, InputPassword, LoginButon, Logo } from './style';
-import { Label } from 'semantic-ui-react';
+import axios from 'axios';
 
 export class Login extends React.Component {
 
@@ -23,8 +23,25 @@ export class Login extends React.Component {
     };
 
     submitValidation = () => {
-         if((this.state.email !== null) && (this.state.password !== null)) this.setState({correct: true});
+         if((this.state.email !== null) && (this.state.password !== null)) 
+            this.setState({correct: true});
+                
     };
+
+    putData = () => {
+        if(this.state.correct){
+            axios({
+                method: 'POST',
+                url: 'http://localhost:62938/api/account/signin',
+                body: {
+                    "Email": this.state.email,
+                    "Password": this.state.password 
+                }
+            }).then((resp) =>{
+                console.log(resp);
+            })
+        } 
+    }
 
     render(){
         return( 
@@ -33,7 +50,7 @@ export class Login extends React.Component {
                     <Logo>teachMe</Logo>
                     <InputEmail placeholder="e-mail" onChange={event => this.emailHandleChange(event.target.value)} error={(this.state.submited && !this.state.emial) ? true : undefined }/>
                     <InputPassword placeholder="hasło" type='password' onChange={event => this.passwordHandleChange(event.target.value)} error={(this.state.submited && !this.state.password) ? true : undefined }/>
-                    <LoginButon primary onMouseOver={this.submitValidation} onClick={() => this.setState({submited: true})}>
+                    <LoginButon primary onMouseOver={this.submitValidation} onClick={() => this.putData}>
                         <Link 
                             to={this.state.correct ? "/profil" : "/logowanie"}
                             style={{display: 'block', height: '100%', color: 'white'}} 
